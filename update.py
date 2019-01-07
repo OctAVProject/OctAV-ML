cc55e47ce1984469580acbc78c1320c495dcf02e
ShellCode33 <shellcode33@protonmail.ch>
2019-01-07 17:19:45 +0100

    Updater squeleton

---
+++ update.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import requests
+
+def retrieve_malicious_domains_and_ips():
+    pass
+
+def retrieve_malware_hashes():
+    pass
+
+def git_push():
+    pass
+
+if __name__ == "__main__":
+    print("OctAV updater is starting...")
+    retrieve_malicious_domains_and_ips()
+    retrieve_malware_hashes()
+    git_push()
+

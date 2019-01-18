5133c36ee4657d1eb38a9c49d915a294a01df2f5
ShellCode33 <shellcode33@protonmail.ch>
2019-01-18 17:34:17 +0100

    Add program arguments processing

--- manager.py
+++ manager.py
@@ -4,6 +4,7 @@
 import os
 import config
 import sync
+import argparse
 
 
 def set_working_directory():
@@ -15,7 +16,22 @@ def set_working_directory():
 
 
 if __name__ == "__main__":
-    config.set_logger()
     set_working_directory()
+
+    parser = argparse.ArgumentParser(description='OctAV Manager : in charge of updating the repository periodically '
+                                                 'and of performing machine learning.')
+    parser.add_argument("-l", "--log-level", choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+                        default="INFO", help="Set the log level")
+
+    parser.add_argument("--ml-only", action="store_true", help="Only perform machine learning.")
+    parser.add_argument("--sync-only", action="store_true", help="Only perform machine learning.")
+
+    args = parser.parse_args()
+
+    if args.ml_only and args.sync_only:
+        parser.error("You cannot specify both ml-only and sync-only at the same time")
+
+    config.set_logger(args.log_level)
+
     sync.all()
     sync.git_push()
